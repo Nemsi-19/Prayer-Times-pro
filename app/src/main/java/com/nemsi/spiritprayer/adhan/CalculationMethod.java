b8b3fecaed33be1e053b1e828d2aee3b1534168f
Mohammed Nemsi <nemsimed@gmail.com>
2026-02-13 00:23:33 +0100

    Create CalculationMethod.java

---
+++ app/src/main/java/com/nemsi/spiritprayer/adhan/CalculationMethod.java
@@ -0,0 +1,45 @@
+package com.nemsi.spiritprayer.adhan;
+
+/**
+ * طرق الحساب المتاحة لأوقات الصلاة
+ */
+public enum CalculationMethod {
+    // رابطة العالم الإسلامي
+    MUSLIM_WORLD_LEAGUE,
+    
+    // الهيئة العامة المصرية للمساحة
+    EGYPTIAN,
+    
+    // جامعة العلوم الإسلامية بكراتشي
+    KARACHI,
+    
+    // أم القرى
+    UMM_AL_QURA,
+    
+    // دبي
+    DUBAI,
+    
+    // قطر
+    QATAR,
+    
+    // الكويت
+    KUWAIT,
+    
+    // تركيا
+    TURKEY,
+    
+    // سنغافورة
+    SINGAPORE,
+    
+    // شمال أمريكا (ISNA)
+    NORTH_AMERICA,
+    
+    // فرنسا
+    FRANCE,
+    
+    // روسيا
+    RUSSIA,
+    
+    // طريقة مخصصة
+    OTHER
+}
