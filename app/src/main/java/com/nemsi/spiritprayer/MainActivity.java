6deac297fb508935ce320270e2c5b684f978b04b
Mohammed Nemsi <nemsimed@gmail.com>
2026-02-12 09:08:31 +0100

    Update MainActivity.java

--- app/src/main/java/com/nemsi/spiritprayer/MainActivity.java
+++ app/src/main/java/com/nemsi/spiritprayer/MainActivity.java
@@ -1,45 +1,60 @@
 package com.nemsi.spiritprayer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.batoulapps.adhan.Coordinates;
 import com.batoulapps.adhan.CalculationParameters;
-import com.batoulapps.adhan.CalculationMethod;
+import com.batoulapps.adhan.Madhab;
 import com.batoulapps.adhan.PrayerTimes;
 import com.batoulapps.adhan.data.DateComponents;
-
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 1. تحديد إحداثيات الموقع (مثال: تونس العاصمة)
+        // 1. تعريف واجهة العرض
+        TextView prayerTimesText = findViewById(R.id.prayerTimesText);
+
+        // 2. التحقق من إذن الموقع (هذا هو الجزء الجديد)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // إذا لم نملك الإذن، نطلبه من المستخدم
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            // إذا كان لدينا الإذن، نقوم بحساب المواقيت (مؤقتاً بإحداثيات تونس)
+            displayPrayerTimes(prayerTimesText);
+        }
+    }
+
+    // دالة حساب وعرض المواقيت
+    private void displayPrayerTimes(TextView textView) {
+        // إحداثيات افتراضية لتونس العاصمة
         Coordinates coordinates = new Coordinates(36.8065, 10.1815);
+        DateComponents dateComponents = DateComponents.from(new Date());
+        CalculationParameters parameters = CalculationParameters.MUSLIM_WORLD_LEAGUE;
+        parameters.madhab = Madhab.SHAFI;
 
-        // 2. ضبط طريقة الحساب (رابطة العالم الإسلامي كمثال)
-        CalculationParameters params = CalculationMethod.MUSLIM_WORLD_LEAGUE.getParameters();
+        PrayerTimes prayerTimes = new PrayerTimes(coordinates, dateComponents, parameters);
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm a", Locale.getDefault());
 
-        // 3. تحديد التاريخ الحالي
-        DateComponents date = DateComponents.from(new Date());
+        String result = "🕌 مواقيت الصلاة برو\n\n" +
+                "الفجر: " + formatter.format(prayerTimes.fajr) + "\n" +
+                "الشروق: " + formatter.format(prayerTimes.sunrise) + "\n" +
+                "الظهر: " + formatter.format(prayerTimes.dhuhr) + "\n" +
+                "العصر: " + formatter.format(prayerTimes.asr) + "\n" +
+                "المغرب: " + formatter.format(prayerTimes.maghrib) + "\n" +
+                "العشاء: " + formatter.format(prayerTimes.isha);
 
-        // 4. تشغيل المحرك لحساب المواقيت
-        PrayerTimes prayerTimes = new PrayerTimes(coordinates, date, params);
-
-        // 5. تنسيق الوقت ليصبح سهل القراءة (مثال: 05:30 PM)
-        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm a");
-
-        String results = "مواقيت الصلاة بتوقيت تونس:\n\n" +
-                "🕋 الفجر: " + formatter.format(prayerTimes.fajr) + "\n" +
-                "☀️ الظهر: " + formatter.format(prayerTimes.dhuhr) + "\n" +
-                "🕌 العصر: " + formatter.format(prayerTimes.asr) + "\n" +
-                "🌅 المغرب: " + formatter.format(prayerTimes.maghrib) + "\n" +
-                "🌙 العشاء: " + formatter.format(prayerTimes.isha);
-        
-        welcomeText.setText(results);
-
+        textView.setText(result);
+    }
+}
