5cb144c7a08819859fa3f847a1630b5320ae5629
Mohammed Nemsi <nemsimed@gmail.com>
2026-02-13 09:32:34 +0100

    Update MainActivity.java

--- app/src/main/java/com/nemsi/spiritprayer/MainActivity.java
+++ app/src/main/java/com/nemsi/spiritprayer/MainActivity.java
@@ -1,33 +1,64 @@
 package com.nemsi.spiritprayer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.nemsi.spiritprayer.adhan.*;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Coordinates coordinates = new Coordinates(34.0, 9.0);
-        PrayerTimes prayerTimes = new PrayerTimes(coordinates, new Date(), CalculationMethod.MUSLIM_WORLD_LEAGUE);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        checkLocationPermission();
+    }
 
+    private void checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+        } else {
+            getLastLocation();
+        }
+    }
+
+    private void getLastLocation() {
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                calculateAndDisplay(location.getLatitude(), location.getLongitude());
+            } else {
+                // إذا فشل الـ GPS، نستخدم موقع تونس كاحتياط
+                calculateAndDisplay(34.0, 9.0);
+            }
+        });
+    }
+
+    private void calculateAndDisplay(double lat, double lon) {
+        Coordinates coordinates = new Coordinates(lat, lon);
+        PrayerTimes prayerTimes = new PrayerTimes(coordinates, new Date(), CalculationMethod.MUSLIM_WORLD_LEAGUE);
         displayAllPrayerTimes(prayerTimes);
     }
 
     private void displayAllPrayerTimes(PrayerTimes prayerTimes) {
         SimpleDateFormat formatter = new SimpleDateFormat("hh:mm a", Locale.getDefault());
-
-        ((TextView) findViewById(R.id.fajr_time)).setText("الفجر: " + formatter.format(prayerTimes.fajr));
-        ((TextView) findViewById(R.id.dhuhr_time)).setText("الظهر: " + formatter.format(prayerTimes.dhuhr));
-        ((TextView) findViewById(R.id.asr_time)).setText("العصر: " + formatter.format(prayerTimes.asr));
-        ((TextView) findViewById(R.id.maghrib_time)).setText("المغرب: " + formatter.format(prayerTimes.maghrib));
-        ((TextView) findViewById(R.id.isha_time)).setText("العشاء: " + formatter.format(prayerTimes.isha));
+        ((TextView) findViewById(R.id.fajr_time)).setText(formatter.format(prayerTimes.fajr));
+        ((TextView) findViewById(R.id.dhuhr_time)).setText(formatter.format(prayerTimes.dhuhr));
+        ((TextView) findViewById(R.id.asr_time)).setText(formatter.format(prayerTimes.asr));
+        ((TextView) findViewById(R.id.maghrib_time)).setText(formatter.format(prayerTimes.maghrib));
+        ((TextView) findViewById(R.id.isha_time)).setText(formatter.format(prayerTimes.isha));
     }
 }
